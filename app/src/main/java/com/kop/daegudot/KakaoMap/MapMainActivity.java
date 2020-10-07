ec21064e5c5cfc91f5da36cd5774422310d7e4bf
soyeonu <thdus1487@naver.com>
2020-10-08 04:29:08 +0900

    To add Kakao Map Library

---
+++ app/src/main/java/com/kop/daegudot/KakaoMap/MapMainActivity.java
@@ -0,0 +1,49 @@
+package com.kop.daegudot.KakaoMap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.kop.daegudot.R;
+
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
+
+public class MapMainActivity extends AppCompatActivity implements MapView.POIItemEventListener {
+    private static final String LOG_TAG = "MapMainActivity";
+    private MapView mMapView;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_main);
+        
+        mMapView = findViewById(R.id.map_view);
+    
+        mMapView.setPOIItemEventListener(this);
+        mMapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(35.871344, 128.601705), true);
+        mMapView.setZoomLevel(6, true);
+    }
+    
+    @Override
+    public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
+    
+    }
+    
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem) {
+    
+    }
+    
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem, MapPOIItem.CalloutBalloonButtonType calloutBalloonButtonType) {
+    
+    }
+    
+    @Override
+    public void onDraggablePOIItemMoved(MapView mapView, MapPOIItem mapPOIItem, MapPoint mapPoint) {
+    
+    }
+}
