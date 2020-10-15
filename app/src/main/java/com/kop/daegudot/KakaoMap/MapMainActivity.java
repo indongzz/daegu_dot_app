7cb5cdf3ede8fdcfbac015222fda273e8a22ae16
soyeonu <thdus1487@naver.com>
2020-10-15 22:02:08 +0900

    To add sample Map Marker 1. custom pin color 2. set Marker name

--- app/src/main/java/com/kop/daegudot/KakaoMap/MapMainActivity.java
+++ app/src/main/java/com/kop/daegudot/KakaoMap/MapMainActivity.java
@@ -2,25 +2,26 @@ package com.kop.daegudot.KakaoMap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
-import android.text.Layout;
 import android.util.Log;
 import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.kop.daegudot.R;
 
+import net.daum.mf.map.api.CalloutBalloonAdapter;
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapMainActivity extends AppCompatActivity implements MapView.POIItemEventListener, View.OnClickListener {
     private static final String LOG_TAG = "MapMainActivity";
@@ -49,6 +50,51 @@ public class MapMainActivity extends AppCompatActivity implements MapView.POIIte
         
         setCategoryBtn();
         setHashBtn();
+    
+        Geocoder geocoder = new Geocoder(this);
+        String[] address = {
+                "대구광역시 중구 남산로 4길 112",
+                "대구광역시 중구 서성로 6-1",
+                "대구광역시 동구동화사 1길 1",
+                "대구광역시 중구 국채보상로 670",
+                "대구광역시 중구 동성로 2길 80"
+        };
+        String[] attractname = { "성모당", "서상돈 고택", "동화사 보사계 유공비", "국채보상운동기념공원", "2.28기념중앙공원" };
+        String[] tel = {
+                "053-250-3000", "053-256-3762",
+                "53-982-0101-2", "053-254-9401",
+                "053-254-9405"
+        };
+        
+        List<Address> list = null;
+        ArrayList<MapPoint.GeoCoordinate> addressList = new ArrayList<>();
+        
+        for (int i = 0; i < 5; i++) {
+            try {
+                list = geocoder.getFromLocationName(address[i], 20);
+                System.out.println(list);
+                for (Address addr : list) {
+                    addressList.add(new MapPoint.GeoCoordinate(addr.getLatitude(), addr.getLongitude()));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("GetLocationName", "location error");
+            }
+        }
+        
+        for (int i = 0; i< 5; i++) {
+            System.out.println("latitude : " + addressList.get(i).latitude + ", longitude : " + addressList.get(i).longitude);
+            MapPOIItem marker = new MapPOIItem();
+            marker.setItemName(attractname[i]);
+            marker.setTag(i);
+            marker.setMapPoint(MapPoint.mapPointWithGeoCoord(addressList.get(i).latitude, addressList.get(i).longitude));
+            marker.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+            marker.setCustomImageResourceId(R.drawable.blue_pin);
+            marker.setSelectedMarkerType(MapPOIItem.MarkerType.CustomImage);
+            marker.setCustomSelectedImageResourceId(R.drawable.yellow_pin);
+    
+            mMapView.addPOIItem(marker);
+        }
     }
     
     public void setCategoryBtn() {
@@ -146,5 +192,23 @@ public class MapMainActivity extends AppCompatActivity implements MapView.POIIte
                 }
                 break;
         }
+    }
+}
+
+class CustomCalloutBalloonAdapter implements CalloutBalloonAdapter {
+    private final View mCalloutBalloon;
+    
+    CustomCalloutBalloonAdapter(View mCalloutBalloon) {
+        this.mCalloutBalloon = mCalloutBalloon;
+    }
+    
+    @Override
+    public View getCalloutBalloon(MapPOIItem mapPOIItem) {
+        return null;
+    }
+    
+    @Override
+    public View getPressedCalloutBalloon(MapPOIItem mapPOIItem) {
+        return null;
     }
 }
