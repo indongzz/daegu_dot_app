f486e81f3211ea42da5efc4268b6374592f20c31
soyeonu <thdus1487@naver.com>
2021-01-07 13:18:02 +0900

    To add AddRecomendation Activity layout

---
+++ app/src/main/java/com/kop/daegudot/Recommend/AddRecommendActivity.java
@@ -0,0 +1,79 @@
+package com.kop.daegudot.Recommend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+import com.kop.daegudot.R;
+
+public class AddRecommendActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "AddRecommendActivity";
+    
+    ImageButton backBtn;
+    EditText mRecommendTitle;
+    EditText mRecommendContent;
+    ChipGroup mChipGroup;
+    RatingBar mRatingBar;
+    Button mConfirmBtn;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_recommend);
+        
+        TextView title = findViewById(R.id.title);
+        title.setText("추천 작성");
+        backBtn = findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(this);
+        
+        mRecommendTitle = findViewById(R.id.et_title);
+        mRecommendContent = findViewById(R.id.et_content);
+        mRatingBar = findViewById(R.id.rating_bar);
+        mConfirmBtn = findViewById(R.id.confirm_btn);
+        mConfirmBtn.setOnClickListener(this);
+        
+        setChipGroup();
+    }
+    
+    public void setChipGroup() {
+        mChipGroup = findViewById(R.id.chip_group);
+        
+        String[] hashs = {"해시태그1", "해시태그2", "해시태그3", "해시태그4"};
+        Chip[] chips = new Chip[hashs.length];
+        
+        for(int i = 0; i < hashs.length; i++) {
+            chips[i] = (Chip)getLayoutInflater().inflate(R.layout.layout_chip_choice, mChipGroup, false);
+            chips[i].setText(hashs[i]);
+            
+            mChipGroup.addView(chips[i]);
+        }
+        
+        mChipGroup.setSingleSelection(true);
+        
+    }
+    
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.backBtn:
+                finish();
+                break;
+            case R.id.confirm_btn:
+                // TODO: 버튼 확인 후 내용 모두 저장
+                Log.i(TAG, "chip group id: " + mChipGroup.getCheckedChipId());
+                Log.i(TAG, "rating bar" + mRatingBar.getRating());
+                
+                finish();
+                break;
+        }
+    }
+}
--- app/src/main/java/com/kop/daegudot/Recommend/RecommendFragment.java
+++ app/src/main/java/com/kop/daegudot/Recommend/RecommendFragment.java
@@ -1,5 +1,6 @@
 package com.kop.daegudot.Recommend;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,26 +8,46 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.kop.daegudot.R;
 
-public class RecommendFragment extends Fragment {
-
-
+public class RecommendFragment extends Fragment implements View.OnClickListener {
+    private static final String TAG = "RecommendFragment";
+    
     public RecommendFragment() {
         // Required empty public constructor
     }
-
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+    
     }
-
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recommend, container, false);
+        View view = inflater.inflate(R.layout.fragment_recommend, container, false);
+        
+        TextView title = view.findViewById(R.id.title);
+        title.setText("추천");
+        
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(this);
+        
+        return view;
+    }
+    
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab:
+                Intent intent = new Intent(getContext(), AddRecommendActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
