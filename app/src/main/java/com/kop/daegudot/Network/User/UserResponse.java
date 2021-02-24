2d769bd37ed6c3258e488d4a01b90c0db24cad4e
HGYEONG <hyeony1070@gmail.com>
2021-02-25 03:05:11 +0900

    1. Add network connection related to User 1.1 sign up 1.2 login 1.3 checking duplicate 1.4 save new user's info 2. Add network connection related to Map 2.1 There is an error so we need to check this part again.

---
+++ app/src/main/java/com/kop/daegudot/Network/User/UserResponse.java
@@ -0,0 +1,11 @@
+package com.kop.daegudot.network;
+
+public class UserResponse {
+    public Long id;
+    public String email;
+    public String nickname;
+    public String password;
+    public String token;
+    public char type;
+
+}
--- app/src/main/java/com/kop/daegudot/network/UserResponse.java
+++
@@ -1,7 +0,0 @@
-package com.kop.daegudot.network;
-
-public class UserResponse {
-    private String email;
-    private String nickname;
-    private char type;
-}
